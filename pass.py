74f4ef66b04cc8fea31da9bfe1786f47f90977fc
Sam Platek <sam.platek@moodys.com>
2022-09-07 08:44:10 -0400

    updating to a different example
    Signed-off-by: Sam Platek <sam.platek@moodys.com>

--- pass.py
+++ pass.py
@@ -9,10 +9,10 @@ import os
 
 name = 'TestDevopsGitGuardian'
 
-access_key1 = 'ASIA2EHZ5M7JX3GAEO5M'
+access_key1 = 'ASIAAKIAFJKR45SAWS5Z'
 
-sec_access_key1 = '3GQ1oJBt0UnfTEt1R/o6i4xVcWl7y6VLtH6FC3HB'
-cli_tok = 'IQoJb3JpZ2luX2VjEGoaCXVzLWVhc3QtMSJHMEUCIQCqb7eWWtTBAzf8XkcSeEWwlgu4rsfypWV4HFx4zyiiDQIgDSU8p29rr'
+sec_access_key1 = 'hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak'
+cli_tok = 'FwoGZXIvYXdzEBAaDLHxhjed4A6ABQplMyKBAd0Jzohb7hRtcvWvjWSNw5bVcn5al0jGu9Cl7W2ijDztOnmLZICjbsFBYgO7mt2J1AM9CO0nrL9qBatm9'
 
 slack_webhook = os.environ.get('SLACK_WEBHOOK_URL', 'hooks.slack.com/services/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')
 
