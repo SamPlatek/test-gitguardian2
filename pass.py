2cb5771547c8483eddef864d5b9d841d7342ec80
Sam Platek <sam.platek@moodys.com>
2022-09-07 08:31:45 -0400

    adding token
    Signed-off-by: Sam Platek <sam.platek@moodys.com>

--- pass.py
+++ pass.py
@@ -8,13 +8,15 @@ import boto3
 
 name = 'TestDevopsGitGuardian'
 
-access_key1 = 'ASIAX52MPYOTPRUCRC22'
+access_key1 = 'ASIA2EHZ5M7JX3GAEO5M'
 
-sec_access_key1 = 'hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak'
+sec_access_key1 = '3GQ1oJBt0UnfTEt1R/o6i4xVcWl7y6VLtH6FC3HB'
+cli_tok = 'IQoJb3JpZ2luX2VjEGoaCXVzLWVhc3QtMSJHMEUCIQCqb7eWWtTBAzf8XkcSeEWwlgu4rsfypWV4HFx4zyiiDQIgDSU8p29rr'
 
 
 
-boto3_session = boto3.session.Session(
+boto3_session = boto3.Session(
   aws_access_key_id = access_key1,
   aws_secret_access_key = sec_access_key1,
+  aws_session_token = cli_tok
 )
