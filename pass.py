62e3a0fdea2f4ea6320a89ca54892fc7230bb996
Sam Platek <sam.platek@moodys.com>
2022-09-07 08:30:32 -0400

    slight change
    Signed-off-by: Sam Platek <sam.platek@moodys.com>

--- pass.py
+++ pass.py
@@ -8,13 +8,13 @@ import boto3
 
 name = 'TestDevopsGitGuardian'
 
-access_key1 = 'ABIAX52MPYOTPRUCRC22'
+access_key1 = 'ASIAX52MPYOTPRUCRC22'
 
 sec_access_key1 = 'hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak'
 
 
 
-boto3_session = boto3.Session(
+boto3_session = boto3.session.Session(
   aws_access_key_id = access_key1,
   aws_secret_access_key = sec_access_key1,
 )
